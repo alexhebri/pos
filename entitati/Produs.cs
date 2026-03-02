dba8ac4bc0542699f70e5e36e0e3846166825999
cti24c202 <cti24c202@CTIDSA12>
2026-03-02 16:49:24 +0200

    Add project files.

---
+++ entitati/Produs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entitati
+{
+    public class Produs
+    {
+        public int CodProdus { get; set; }
+        public string Denumire { get; set; }
+        public string Producator { get; set; }
+
+        public Produs(int codProdus, string denumire, string producator)
+        {
+            CodProdus = codProdus;
+            Producator = producator;
+            Denumire = denumire;
+        }
+
+        public void Afisare()
+        {
+            Console.WriteLine(CodProdus + " " + Denumire + " " + Producator);
+        }
+
+        public static int CautaProdus(Produs[] v, int n, int codCautat)
+        {
+            int ok = 0; //pp. produsul nu se afla in lista
+            for(int i = 0; i < n; i++)
+            {
+                if (v[i].CodProdus == codCautat)
+                    ok = 1; //am gasit produsul in lista
+            }
+            return ok;
+        }
+
+
+    }
+}
---
+++ entitati/Serviciu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entitati
+{
+    public class Serviciu
+    {
+        public int CodServiciu { get; set; }
+        public string Denumire { get; set; }
+        public double Pret { get; set; }
+        public int DurataReparatie { get; set; }
+
+        public Serviciu(int codServiciu, string denumire, double pret, int durataReparatie)
+        {
+            CodServiciu = codServiciu;
+            Denumire = denumire;
+            Pret = pret;
+            DurataReparatie = durataReparatie;
+        }
+
+        public void Afisare()
+        {
+            Console.WriteLine(CodServiciu + " " + Denumire + " " + Pret + " " + DurataReparatie);
+        }
+
+        public static int CautaServiciu(Produs[] v, int n, int codCautat)
+        {
+            int ok = 0; //pp. serviciu nu se afla in lista
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i].CodProdus == codCautat)
+                    ok = 1; //am gasit serviciul in lista
+            }
+            return ok;
+        }
+    }
+}
